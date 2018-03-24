1504abbf371ccb9b6b3e36e99ed876e97b941ff2
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2018-03-25 01:48:05 +0800

    Memuat Atribut dan behavior

--- src/tugas1pbo/Baju.java
+++ src/tugas1pbo/Baju.java
@@ -10,5 +10,18 @@ package tugas1pbo;
  * @author puden
  */
 public class Baju {
+    String kain;
+    String Warna;
+    String Kancing;
+    String motiv;
+    String ukuran;
     
+    void cetakinfo(){
+    System.out.println("Kain :"+kain);
+    System.out.println("Warna :"+Warna);
+    System.out.println("Kancing :"+Kancing);
+    System.out.println("Motiv :"+motiv);
+    System.out.println("Ukuran :"+ukuran);
+    }
+    }
 }
